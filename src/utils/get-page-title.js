8d6be24187bf274da8899af0fcc02acf2b4890dc
pburkoti@genixventures.com <pburkoti@genixventures.com>
2023-05-15 13:51:30 +0530

    Ready for run

--- src/utils/get-page-title.js
+++ src/utils/get-page-title.js
@@ -1,7 +1,7 @@
 import defaultSettings from '@/settings'
 import i18n from '@/lang'
 
-const title = defaultSettings.title || 'GiiBee CMS 管理系统'
+const title = defaultSettings.title || 'Cloud Component'
 
 export default function getPageTitle(key) {
   const hasKey = i18n.te(`route.${key}`)
